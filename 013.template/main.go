4e5b0abbcb09ee289332cd33eac8bf67c213fbac
Leon Ji <mgleon08@gmail.com>
2019-04-02 09:22:43 +0800

    013.template

---
+++ 013.template/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+type Todo struct {
+	Title string
+	Done  bool
+}
+
+type TodoPageData struct {
+	PageTitle string
+	Todos     []Todo
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("./layout.html"))
+	data := TodoPageData{
+		PageTitle: "My TODO list",
+		Todos: []Todo{
+			{Title: "Task 1", Done: false},
+			{Title: "Task 2", Done: true},
+			{Title: "Task 3", Done: true},
+		},
+	}
+	tmpl.Execute(w, data)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
